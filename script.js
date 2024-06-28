46466c33cb179c573091787f42be86c6989eb3e8
Fathi <Fathiatsiilah18@gmail.com>
2024-06-28 16:05:48 +0700

    calcu

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function onSubmit() { 
-    const day = parseInt(document.getElementById('day').value);
-        const month = parseInt(document.getElementById('month').value);
-        const year = parseInt(document.getElementById('year').value);
-    
-        if (!isNaN(day) && !isNaN(month) && !isNaN(year)) {
+  const day = parseInt(document.getElementById('day').value);
+  const month = parseInt(document.getElementById('month').value);
+  const year = parseInt(document.getElementById('year').value);
+  
+  try {
+      validateDate(day, month, year);
+      
+      if (!isNaN(day) && !isNaN(month) && !isNaN(year)) {
           const birthDate = new Date(year, month - 1, day);
           const currentDate = new Date();
           let years = currentDate.getFullYear() - birthDate.getFullYear();
@@ -11,18 +14,28 @@ function onSubmit() {
           let days = currentDate.getDate() - birthDate.getDate();
           
           if (days < 0) {
-            months--;
-            days += new Date(year, month, 0).getDate();
+              months--;
+              days += new Date(year, month, 0).getDate();
           }
           if (months < 0) {
-            years--;
-            months += 12;
+              years--;
+              months += 12;
           }
           
           document.getElementById('years').textContent = years;
           document.getElementById('months').textContent = months;
           document.getElementById('days').textContent = days;
-        } else {
+      } else {
           alert('Please enter valid date values.');
-        }
+      }
+  } catch (error) {
+      alert(error.message);
+  }
+}
+
+function validateDate(day, month, year) {
+  const date = new Date(year, month - 1, day);
+  if (date.getFullYear() !== year || date.getMonth() + 1 !== month || date.getDate() !== day) {
+      throw new Error("Must be a valid day");
+  }
 }
